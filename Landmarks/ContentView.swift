77b0bc103ecb799dce855054aef0c9d8a67e0d79
岩永 彩里 <iwanaga.sari@moneyforward.co.jp>
2019-08-13 16:59:40 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 岩永 彩里 on 2019/08/13.
+//  Copyright © 2019 岩永 彩里. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
